1be15d0d77b094f8bd9422b81d4d96a669f45aba
Shikamaru-kun <79463536+shikamaru-kun1@users.noreply.github.com>
2025-05-10 00:44:06 +0100

    Segundo commit
    - Criação dinâmica de pastas
    - Copiar ficheiros de uma pasta de fora para a pasta criada

---
+++ DbaseToExcelConverter/DbfToXlsConvTester/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbfToXlsConvTester
+{
+    internal class Program
+    {
+        private static Resources resources = new Resources();
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                // 1. Obter diretório do projeto
+                string projectPath = resources.GetProjectDirectory();
+                Console.WriteLine($"caminho: {projectPath}");
+
+                // 2. Criar estrutura de pastas
+                string targetPath = Path.Combine(projectPath, "DataRepo", "In");
+
+                resources.CreateDirectoryIfNotExists(targetPath);
+
+                // 3. Copiar arquivos
+                string sourcePath = "C:/Users/Administrator/Projectos EMJIT/Integration/DbfToExcelConverter/DbfToExcelConverter/Tests/TestFiles/Repo/DbfSamples";
+                resources.CopyFiles(sourcePath, targetPath);
+
+                //Console.WriteLine("Operação concluída com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro: {ex.Message}");
+            }
+            //DbfToExcel dbfToExcel = new DbfToExcel();
+            //string dbfFile = @"C:\dados\arquivo.dbf";
+            //string excelFile = @"C:\export\dados.xls";
+
+            //dbfToExcel.ConvertDbfToExcel(dbfFile, excelFile);
+        }
+    }
+}
---
+++ DbaseToExcelConverter/DbfToXlsConvTester/Resources.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbfToXlsConvTester
+{
+    public class Resources
+    {
+        public Resources() { }
+
+        public string GetProjectDirectory()
+        {
+            // Para projectos .NET Core (bin/Debug/netX.Y)
+            string currentDir = Directory.GetCurrentDirectory();
+            Console.WriteLine($"path: {Directory.GetParent(currentDir).Parent.Parent.FullName}");
+            return Directory.GetParent(currentDir).Parent.Parent.FullName;
+            //return currentDir;
+        }
+
+        public void CreateDirectoryIfNotExists(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+                Console.WriteLine($"Diretório criado: {path}");
+            }
+        }
+
+        public void CopyFiles(string sourceDir, string targetDir)
+        {
+            var files = Directory.EnumerateFiles(sourceDir);
+
+            foreach (var file in files)
+            {
+                string destFile = Path.Combine(targetDir, Path.GetFileName(file));
+                File.Copy(file, destFile, overwrite: true);
+                Console.WriteLine($"Copiado: {Path.GetFileName(file)}");
+            }
+        }
+    }
+}
